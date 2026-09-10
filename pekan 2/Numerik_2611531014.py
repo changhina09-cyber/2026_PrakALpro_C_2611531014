294a773aa158d47cf4f307f4032defe33333a9f0
asiyahshahiradelhan-sketch <asiyahshahiradelhan@gmail.com>
2026-09-10 12:14:52 +0700

    praktikum pekan 2

---
+++ pekan 2/Numerik_2611531014.py
@@ -0,0 +1,13 @@
+#buat file dengan nama Numerik_2611531014.py
+#nama variabel ditambah 4 digit terakhir NIM contoh: a_1014
+#create a variable with numeric type value.
+a=100
+print ("The type of variable having value",a,"is",type(a))
+
+#create a variable with float value.
+b=10.2345
+print("The type of variable having value",b,"is",type(b))
+
+#create a variable with complex value.
+c=100+3j
+print("The type of variable having value",c,"is", type(c))
---
+++ pekan 2/string_2611531014.py
@@ -0,0 +1,15 @@
+#buat file dengan nama String_2611531014
+#nama variabel ditambah 4 digit nim terakhir contoh: Ghina_1014
+#ganti programnya dan data masing2
+nama = "Ghina"
+jenis_kelamin = 'P'
+alamat = """
+    Gulai Bancah
+    kec.mandiangin koto selayan
+    kota Bukittinggi
+"""
+agama = 'islam'
+print("Nama saya adalah: ", nama)
+print("Jenis kelamin saya adalah: ", jenis_kelamin)
+print("Alamat saya adalah: ", alamat)
+print("Agama saya adalah: ", agama)
